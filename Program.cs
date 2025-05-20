d25303cfd8b2bd032b9db97ba03dd96f373b2ff9
ArdillaV21 <fedevalle21@gmail.com>
2025-05-20 12:35:45 -0300

    primer avance

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿int a;
+int b;
+a = 10;
+b = a;
+Console.WriteLine("Valor de a:" + a);
+Console.WriteLine("Valor de a:" + b);
+
+
+string? invertir;
+Console.Write("Ingrese un numero:");
+invertir = Console.ReadLine();
+int numero = int.Parse(invertir);
+
+int invertido = 0;
+while (numero > 0)
+{
+    int dig = numero % 10;
+    invertido = (invertido * 10) + dig;
+    numero = numero / 10;
+}
+Console.Write("El numero invertido:"+invertido);
+
