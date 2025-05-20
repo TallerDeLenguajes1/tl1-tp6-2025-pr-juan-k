887b3d05e092df11db2f58c178453bf7cbce04ac
juan-k <djjuankalexis18@gmail.com>
2025-05-20 13:13:18 -0300

    CalculadoraV1

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ Console.WriteLine("Valor de a:" + b);
 
 
 
-string? invertir;
+/*string? invertir;
 Console.Write("Ingrese un numero:");
 invertir = Console.ReadLine();
 int numero = int.Parse(invertir);
@@ -19,5 +19,62 @@ while (numero > 0)
     invertido = (invertido * 10) + dig;
     numero = numero / 10;
 }
-Console.Write("El numero invertido:"+invertido);
+Console.Write("El numero invertido:"+invertido);*/
+
+
+Console.Write("Ingrese el primer numero:");
+string? dato1 = Console.ReadLine();
+int num1 = int.Parse(dato1);
+
+
+Console.Write("Ingrese el segundo numero:");
+string? dato2 = Console.ReadLine();
+int num2 = int.Parse(dato2);
+
+Console.Write("Ingrese que quiere hacer: 1-Sumar, 2-Resta, 3-Multiplicar, 4-Dividir");
+
+string? dato = Console.ReadLine();
+int eleccion = int.Parse(dato);
+
+int resultado = 0;
+
+switch (eleccion)
+{
+    case 1:
+        resultado = Sumar(num1, num2);
+        break;
+
+    case 2:
+        resultado = Restar(num1, num2);
+        break;
+
+    case 3:
+        resultado = Multiplicar(num1, num2);
+        break;
+
+    case 4:
+        resultado = Dividir(num1, num2);
+        break;
+    
+    default:
+        break;
+}
+Console.Write(resultado);
+
+int Sumar(int a, int b)
+{
+    return a + b;
+}
+int Restar(int a, int b)
+{
+    return a - b;
+}
+int Multiplicar(int a, int b)
+{
+    return a * b;
+}
+int Dividir(int a, int b)
+{
+    return a / b;
+}
 
