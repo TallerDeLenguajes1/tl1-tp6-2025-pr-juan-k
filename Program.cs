3783535303de19ea907c6ec2091ef6e85629f7e6
pr-juan-k <djjuankalexis18@gmail.com>
2025-05-26 23:24:55 -0300

    avance ultimo punto

--- Program.cs
+++ Program.cs
@@ -9,85 +9,7 @@ Console.WriteLine("Valor de a:" + b);
 
 
 
-/*string? invertir;
-Console.Write("Ingrese un numero:");
-invertir = Console.ReadLine();
-int numero = int.Parse(invertir);
-
-int invertido = 0;
-while (numero > 0)
-{
-    int dig = numero % 10;
-    invertido = (invertido * 10) + dig;
-    numero = numero / 10;
-}
-Console.Write("El numero invertido:"+invertido);*/
 /*
-int num1 = 0,num2 = 0,eleccion = 0;
-do
-{
-    Console.Write("Ingrese el primer numero:");
-    string? dato1 = Console.ReadLine();
-    num1 = int.Parse(dato1);
-} while (num1 == 0 );
-
-
-do
-{
-    Console.Write("Ingrese el segundo numero:");
-    string? dato2 = Console.ReadLine();
-    num2 = int.Parse(dato2);
-} while (num2 == 0);
-
-do
-{
-    
-    Console.Write("1-Sumar, 2-Resta, 3-Multiplicar, 4-Dividir \n Ingrese que quiere hacer: ");
-    string? dato = Console.ReadLine();
-    eleccion = int.Parse(dato);
-} while (eleccion < 1 || eleccion > 4);
-
-int resultado = 0;
-
-switch (eleccion)
-{
-    case 1:
-        resultado = Sumar(num1, num2);
-        break;
-
-    case 2:
-        resultado = Restar(num1, num2);
-        break;
-
-    case 3:
-        resultado = Multiplicar(num1, num2);
-        break;
-
-    case 4:
-        resultado = Dividir(num1, num2);
-        break;
-    
-    default:
-        break;
-}
-Console.Write("El resultado es:{0} \n\n",resultado);
-
-int Sumar(int a, int b)
-{
-    return a + b;
-}
-int Restar(int a, int b)
-{
-    return a - b;
-}
-int Multiplicar(int a, int b)
-{
-    return a * b;
-}
-int Dividir(int a, int b)
-{
-    return a / b;
-}
 Console.WriteLine("El valor absoluto de:" + resultado + " es " + resultado);
 int cuadrado = resultado * resultado;
 Console.WriteLine("El cuadrado de:"+resultado +" es "+cuadrado);
@@ -136,3 +58,107 @@ if (num_1 > num_2)
     }else{
         Console.WriteLine("El texto no fue encontrado en la cadena");
     }
+//Punto 4
+
+int num1 = 0,num2 = 0,eleccion = 0;
+do
+{
+    Console.Write("Ingrese el primer numero:");
+    string? dato1 = Console.ReadLine();
+    num1 = int.Parse(dato1);
+} while (num1 == 0 );
+
+
+do
+{
+    Console.Write("Ingrese el segundo numero:");
+    string? dato2 = Console.ReadLine();
+    num2 = int.Parse(dato2);
+} while (num2 == 0);
+
+do
+{
+    
+    Console.Write("1-Sumar, 2-Resta, 3-Multiplicar, 4-Dividir \n Ingrese que quiere hacer: ");
+    string? dato = Console.ReadLine();
+    eleccion = int.Parse(dato);
+} while (eleccion < 1 || eleccion > 4);
+
+int resultado = 0;
+
+switch (eleccion)
+{
+    case 1:
+        resultado = Sumar(num1, num2);
+        break;
+
+    case 2:
+        resultado = Restar(num1, num2);
+        break;
+
+    case 3:
+        resultado = Multiplicar(num1, num2);
+        break;
+
+    case 4:
+        resultado = Dividir(num1, num2);
+        break;
+    
+    default:
+        break;
+}
+Console.WriteLine("\nNúmero 1: " + num1.ToString());
+Console.WriteLine("Número 2: " + num2.ToString());
+Console.WriteLine("Resultado: " + resultado.ToString());
+
+int Sumar(int a, int b)
+{
+    return a + b;
+}
+int Restar(int a, int b)
+{
+    return a - b;
+}
+int Multiplicar(int a, int b)
+{
+    return a * b;
+}
+int Dividir(int a, int b)
+{
+    return a / b;
+}
+//punto 5
+foreach (char c in cadena_1)
+{
+    Console.WriteLine(c);
+}
+//punto 6
+Console.Write("Ingrese la palabra que desea buscar: ");
+string? palabra = Console.ReadLine();
+
+int index = 0;
+int contador = 0;
+List<int> posiciones = new List<int>();
+
+while ((index = cadena_concatenada.IndexOf(palabra, index)) != -1)
+{
+    contador++;
+    posiciones.Add(index);
+    index += palabra.Length; 
+}
+
+if (contador > 0)
+{
+    Console.WriteLine($"La palabra '{palabra}' apareció {contador} vez/veces.");
+    Console.WriteLine("Posiciones donde aparece (índice de inicio):");
+
+    foreach (int pos in posiciones)
+    {
+        Console.WriteLine(pos);
+    }
+}
+else
+{
+    Console.WriteLine($"La palabra '{palabra}' no se encontró en el texto.");
+}
+
